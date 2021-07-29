2d962799b2ef853018060e65d4692a4c5babca64
abhinum95 <abhinum95@gmail.com>
2021-07-29 15:36:53 +0530

    code for authorList with autocomplete update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [authorName, setAuthorName] = useState("");
+
+  useEffect(async () => {
+    const url = "https://jsonmock.hackerrank.com/api/articles";
+    await fetch(url)
+      .then(async (res) => await res.json())
+      .then((res) => {
+        setList(res.data);
+        getautocompleteList();
+      });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //   getautocompleteList();
+  };
+
+  const getautocompleteList = () => {
+    if (authorName === "") return []; // If no input show empty list
+
+    let filtered = list.filter((item) => {
+      console.log(item.author.indexOf(authorName) === 0);
+      return item.author.indexOf(authorName) === 0;
+    });
+    setFilteredList(() => filtered);
+  };
+
+  const handleChange = (e) => {
+    setAuthorName(() => e.target.value);
+    if (e.target.value === "") {
+      setFilteredList(() => []);
+    } else {
+      getautocompleteList();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} />
+        <div className="autosuggest">
+          {filteredList.map((item) => (
+            <div key={item.created_at}>{item.author}</div>
+          ))}
+        </div>
+        <button type="submit">Search</button>
+      </form>
+      {list.map((item) => (
+        <div key={item.created_at}>{item.author}</div>
+      ))}
     </div>
   );
 }
